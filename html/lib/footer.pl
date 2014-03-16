04c74dfe1fe1f4362a8c81cb90b6e7b412e06451
Oliver Kurmis <oliver@kurmis.com>
2014-03-16 18:06:54 +0100

    redesign homepage layout (css, no tables)

--- html/lib/footer.pl
+++ html/lib/footer.pl
@@ -1,39 +1,10 @@
 print '
-<!-- /Content -->
-					</TD>
-				</TR>				
-			</TABLE>
-		</TD>
-		<TD BGCOLOR="#000000"><IMG SRC="images/p0.gif" 
-		BORDER=0 WIDTH=1 HEIGHT=1 HSPACE=0 VSPACE=0></TD>
-	</TR>
-	<TR>
-		<TD COLSPAN=3 BGCOLOR="#000000"><IMG 
-		SRC="/images/p0.gif" BORDER=0 WIDTH=1 HEIGHT=1 
-		HSPACE=0 VSPACE=0></TD>
-	</TR>
-	<TR><TD COLSPAN=4 BGCOLOR="#663300" VALIGN="TOP"><IMG 
-	SRC="/images/pb.gif" BORDER=0 WIDTH=3 HEIGHT=1 HSPACE=0 
-	VSPACE=0></TD></TR>
-	<TR><TD COLSPAN=5 BGCOLOR="#996600" VALIGN="TOP"><IMG 
-	SRC="/images/pb.gif" BORDER=0 WIDTH=4 HEIGHT=1 HSPACE=0 
-	VSPACE=0></TD></TR>
-	<TR><TD COLSPAN=6 BGCOLOR="#999900" VALIGN="TOP"><IMG 
-	SRC="/images/pb.gif" BORDER=0 WIDTH=4 HEIGHT=1 HSPACE=0 
-	VSPACE=0></TD></TR>
-	<TR><TD COLSPAN=7 BGCOLOR="#CC9900" VALIGN="TOP"><IMG 
-	SRC="/images/pb.gif" BORDER=0 WIDTH=5 HEIGHT=1 HSPACE=0 
-	VSPACE=0></TD></TR>
-	<TR>
-		<TD COLSPAN=3 BGCOLOR="#FFCC00" ALIGN="CENTER" 
-		VALIGN="TOP">&nbsp;</TD>
-		<TD COLSPAN=4 BGCOLOR="#FFCC00"><IMG 
-		SRC="/images/p.gif" BORDER=0 WIDTH=1 HEIGHT=1 
-		HSPACE=0 VSPACE=0></TD>
-	</TR>
-</TABLE>
-<div class="foot">served by <a href="https://github.com/oliworx/nanohttp">nanoHttp</a></div>
-</CENTER>
-</BODY>
-</HTML>
+	<!-- /Content -->
+	<div class="footer">
+		served by 
+		<a href="https://github.com/oliworx/nanohttp">nanoHttp</a>
+	</div>
+	</div><!-- /Page -->
+</body>
+</html>
 ';
